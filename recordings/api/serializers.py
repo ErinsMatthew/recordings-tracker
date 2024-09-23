cbf3346197c8684822f982f9fa9be530d49d2c3c
Matthew W. Foster <github@matthewanderin.com>
2024-09-23 14:55:51 -0500

    Fixed typo and removed hyperlink.

--- recordings/api/serializers.py
+++ recordings/api/serializers.py
@@ -1,10 +1,10 @@
-from argparse import FileType
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
 from .models import (
     Equipment,
     EquipmentType,
+    FileType,
     Location,
     Platform,
     Program,
@@ -19,97 +19,97 @@ from .models import (
 )
 
 
-class ProgramSerializer(serializers.HyperlinkedModelSerializer):
+class ProgramSerializer(serializers.ModelSerializer):
     class Meta:
         model = Program
         fields = "__all__"
 
 
-class LocationSerializer(serializers.HyperlinkedModelSerializer):
+class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = "__all__"
 
 
-class SeasonSerializer(serializers.HyperlinkedModelSerializer):
+class SeasonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Season
         fields = "__all__"
 
 
-class SeasonGroupSerializer(serializers.HyperlinkedModelSerializer):
+class SeasonGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = SeasonGroup
         fields = "__all__"
 
 
-class ShowSerializer(serializers.HyperlinkedModelSerializer):
+class ShowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Show
         fields = "__all__"
 
 
-class PlatformSerializer(serializers.HyperlinkedModelSerializer):
+class PlatformSerializer(serializers.ModelSerializer):
     class Meta:
         model = Platform
         fields = "__all__"
 
 
-class ShowPlatformSerializer(serializers.HyperlinkedModelSerializer):
+class ShowPlatformSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowPlatform
         fields = "__all__"
 
 
-class EquipmentTypeSerializer(serializers.HyperlinkedModelSerializer):
+class EquipmentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = EquipmentType
         fields = "__all__"
 
 
-class EquipmentSerializer(serializers.HyperlinkedModelSerializer):
+class EquipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipment
         fields = "__all__"
 
 
-class ShowEquipmentSerializer(serializers.HyperlinkedModelSerializer):
+class ShowEquipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowEquipment
         fields = "__all__"
 
 
-class FileTypeSerializer(serializers.HyperlinkedModelSerializer):
+class FileTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileType
         fields = "__all__"
 
 
-class ShowFileSerializer(serializers.HyperlinkedModelSerializer):
+class ShowFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowFile
         fields = "__all__"
 
 
-class SongSerializer(serializers.HyperlinkedModelSerializer):
+class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = "__all__"
 
 
-class ShowSongSerializer(serializers.HyperlinkedModelSerializer):
+class ShowSongSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowSong
         fields = "__all__"
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["url", "username", "email", "groups"]
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ["url", "name"]
