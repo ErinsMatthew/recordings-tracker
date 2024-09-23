c1b570dcd26a2183a462303d49431538bc61dcd1
Matthew W. Foster <github@matthewanderin.com>
2024-09-23 15:09:35 -0500

    Updated.

--- recordings/api/models.py
+++ recordings/api/models.py
@@ -41,8 +41,20 @@ class SeasonGroup(models.Model):
     has_tristan = models.BooleanField(default=True)
     has_erin = models.BooleanField(default=False)
 
+    @property
+    def full_name(self):
+        full_name_ = []
+
+        if self.name:
+            full_name_.append(self.name)
+            full_name_.append(" - ")
+
+        full_name_.append(self.season.name)
+
+        return "".join(full_name_)
+
     def __str__(self):
-        return f"{self.name} - {self.season.name}"
+        return self.full_name
 
 
 class Show(models.Model):
@@ -51,16 +63,20 @@ class Show(models.Model):
     date = models.DateField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     recorded = models.BooleanField(default=False)
-    comments = models.CharField(max_length=4096)
+    comments = models.CharField(max_length=4096, default="")
     sequence = models.PositiveSmallIntegerField(null=True)
 
+    @property
+    def full_name(self):
+        return "".join([self.season_group.full_name, " - ", self.name])
+
     def __str__(self):
-        return self.name
+        return self.full_name
 
 
 class Platform(models.Model):
     name = models.CharField(max_length=128)
-    url_pattern = models.CharField(max_length=256)
+    url_pattern = models.CharField(max_length=256, default="")
 
     def __str__(self):
         return self.name
@@ -118,10 +134,8 @@ class ShowFile(models.Model):
     file_type = models.ForeignKey(FileType, on_delete=models.CASCADE)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     sequence = models.PositiveSmallIntegerField(null=True)
-    icloud_local_path = models.CharField(
-        max_length=4096
-    )  # see PATH_MAX in <linux/limits.h>
-    icloud_url = models.URLField()
+    icloud_local_path = models.CharField(max_length=4096,default="")
+    icloud_url = models.URLField(default="")
     file_size = models.PositiveIntegerField()
     file_stats_json = models.JSONField()
 
@@ -150,6 +164,6 @@ class ShowSong(models.Model):
 
     def __str__(self):
         return self.show.name
-    
+
     class Meta:
         unique_together = ["show", "song"]
